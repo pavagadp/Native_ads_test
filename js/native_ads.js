074a3d7bf89817e4dd7a89167e453a69c2202525
VarunSK <Varun.Krishnamurthy@timeinc.com>
2016-10-19 13:39:55 +0530

    Update native_ads.js

--- js/native_ads.js
+++ js/native_ads.js
@@ -18,7 +18,7 @@ function elementInViewport(el) {
   );
 }
 document.onscroll = function(){
-var d = document.getElementById("iframe");
+var d = document.getElementsByClassName("card");
     if(elementInViewport(d)) {
            d.classList.add('zoomad');
         d.classList.remove('zoomoutad');
