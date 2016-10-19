7934dd517f24462c768393e018f99d3c8463c433
VarunSK <Varun.Krishnamurthy@timeinc.com>
2016-10-19 12:52:49 +0530

    Update native_ads.js

--- js/native_ads.js
+++ js/native_ads.js
@@ -1,1 +1,30 @@
+function elementInViewport(el) {
+   var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
 
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top < (window.pageYOffset + window.innerHeight) &&
+    left < (window.pageXOffset + window.innerWidth) &&
+    (top + height) > window.pageYOffset &&
+    (left + width) > window.pageXOffset
+  );
+}
+document.onscroll = function(){
+var d = document.getElementById("testi");
+    if(elementInViewport(d)) {
+           d.classList.add('zoomad');
+        d.classList.remove('zoomoutad');
+        
+    } else {
+         d.classList.add('zoomoutad');
+        d.classList.remove('zoomad');
+    }
+ };
