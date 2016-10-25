f110fe78d933af6939f27b923dd137dbd05604f2
VarunSK <Varun.Krishnamurthy@timeinc.com>
2016-10-25 14:50:17 +0530

    Update native_ad.js

--- js/native_ad.js
+++ js/native_ad.js
@@ -1,16 +1,13 @@
-var abc="xyz";
 function elementInViewport(el) {
    var top = el.offsetTop;
   var left = el.offsetLeft;
   var width = el.offsetWidth;
   var height = el.offsetHeight;
-
   while(el.offsetParent) {
     el = el.offsetParent;
     top += el.offsetTop;
     left += el.offsetLeft;
   }
-
   return (
     top < (window.pageYOffset + window.innerHeight) &&
     left < (window.pageXOffset + window.innerWidth) &&
@@ -18,15 +15,33 @@ function elementInViewport(el) {
     (left + width) > window.pageXOffset
   );
 }
-document.onscroll = function(){
-   var d = document.getElementById("test");
-   var win = document.getElementById("iframe").contentWindow;
-    if(elementInViewport(d)) {
-       win.postMessage('Shoot', 'https://varunsk.github.io/');
-       //d.classList.add('zoomad');
-       //d.classList.remove('zoomoutad');
-    } else {
-         //d.classList.add('zoomoutad');
-        //d.classList.remove('zoomad');
-    }
-   };
+var scrollTimeout = 0;
+var lastScrollTop = 0;
+    $(window).scroll(function () {
+      if(scrollTimeout){
+        clearTimeout(scrollTimeout);
+      }
+       else{
+        scrollTimeout = setTimeout(function(){
+          //inform to stop animation
+          var win = document.getElementById("iframe").contentWindow;
+          win.postMessage('Stop animation','http://localhost');
+        }, 100);
+      }
+        scrollHandler();
+    });
+
+scrollHandler = function() {
+  var d = document.getElementById("test");
+  var win = document.getElementById("iframe").contentWindow;
+  if(elementInViewport(d)){
+  st = $(this).scrollTop();
+  if(st < lastScrollTop) {
+      win.postMessage('Up','https://varunsk.github.io');
+      }
+  else {
+      win.postMessage('Down','https://varunsk.github.io');
+      }
+  lastScrollTop = st;
+  }
+}
